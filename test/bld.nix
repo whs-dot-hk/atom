262ade1f010bc089d70012e8c5b50b03c9dcb2c0
David Arnold <dgx.arnold@gmail.com>
2024-08-04 13:24:02 +0200

    Update bld.nix

--- test/bld.nix
+++ test/bld.nix
@@ -1,7 +1,9 @@
 let
-  mod =
-    import ../. { }
-      # added to test implicit path conversion when path is a string
-      (builtins.toPath ./bld);
+  compose = import ../.;
+  mod = compose { } (
+    # added to test implicit path conversion when path is a string
+    builtins.toPath
+    ./bld
+  );
 in
 builtins.deepSeq mod mod
