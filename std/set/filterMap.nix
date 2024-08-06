e69184ecee205557e31c86f6310818b45d440302
Timothy DeHerrera <tim@nrdxp.dev>
2024-08-06 14:08:15 -0600

    doc(std): document `std.fix`
    copied from upstream

--- std/set/filterMap.nix
+++ std/set/filterMap.nix
@@ -19,8 +19,9 @@
 
   # Parameters
 
-  - [f] A function that takes an attribute name and an attribute value, and returns either a new key-value pair or a single value. If a single value is returned, the original key is preserved. If a key-value pair is returned, the new key is used, allowing for key updates. If null is returned, the item is removed from the final set.
-  - [set] The attribute set to filter and map.
+  - `f`: A function that takes an attribute name and an attribute value, and returns either a new key-value pair or a single value. If a single value is returned, the original key is preserved. If a key-value pair is returned, the new key is used, allowing for key updates. If null is returned, the item is removed from the final set.
+
+  - `set`: The attribute set to filter and map.
 */
 f: set:
 std.foldl' (
