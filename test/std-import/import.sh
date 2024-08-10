a529a0a4807fe261921d49d4a9300fa42f3a2f4b
David <dgx.arnold@gmail.com>
2024-08-10 16:15:38 +0200

    chore: rearrange test order

--- test/std-import/import.sh
+++ test/std-import/import.sh
@@ -3,35 +3,35 @@
 set -ex
 
 # defaults
+f="$(nix eval -f import.nix default.compose)"
+[[ "$f" == '[ "std" ]' ]]
 f="$(nix eval -f import.nix default.std)"
 [[ "$f" == true ]]
 f="$(nix eval -f import.nix default.lib)"
 [[ "$f" == false ]]
-f="$(nix eval -f import.nix default.compose)"
-[[ "$f" == '[ "std" ]' ]]
 f="$(nix eval -f import.nix default.sanity)"
 [[ "$f" == true ]]
 
 # explicit
+f="$(nix eval -f import.nix explicit.compose)"
+[[ "$f" == '[ "std" ]' ]]
 f="$(nix eval -f import.nix explicit.std)"
 [[ "$f" == true ]]
 f="$(nix eval -f import.nix explicit.lib)"
 [[ "$f" == false ]]
-f="$(nix eval -f import.nix explicit.compose)"
-[[ "$f" == '[ "std" ]' ]]
 
 # no std set
+f="$(nix eval -f import.nix noStd.compose)"
+[[ "$f" == '[ ]' ]]
 f="$(nix eval -f import.nix noStd.std)"
 [[ "$f" == false ]]
 f="$(nix eval -f import.nix noStd.lib)"
 [[ "$f" == false ]]
-f="$(nix eval -f import.nix noStd.compose)"
-[[ "$f" == '[ ]' ]]
 
 # no std set
+f="$(nix eval -f import.nix withNixpkgsLib.stdF)"
+[[ "$f" == '[ "lib" "pkg_lib" ]' ]]
 f="$(nix eval -f import.nix withNixpkgsLib.std)"
 [[ "$f" == true ]]
 f="$(nix eval -f import.nix withNixpkgsLib.lib)"
 [[ "$f" == true ]]
-f="$(nix eval -f import.nix withNixpkgsLib.stdF)"
-[[ "$f" == '[ "lib" "pkg_lib" ]' ]]
